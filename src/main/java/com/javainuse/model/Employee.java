f8ff843e25a90bf80ead4b18c5a0e4eed38179b4
sharmarohit04 <72381613+sharmarohit04@users.noreply.github.com>
2021-08-16 09:23:37 +0530

    initial commit

---
+++ src/main/java/com/javainuse/model/Employee.java
@@ -0,0 +1,53 @@
+package com.javainuse.model;
+
+public class Employee {
+	private String branch;
+	private String empId;
+	private String name;
+	private String designation;
+	private double salary;
+
+	public Employee() {
+	}
+	
+	public String getBranch() {
+		return branch;
+	}
+
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public String getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+
+}
